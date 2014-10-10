62b320a5d3b947b7755820bfdfb225f383ff1f5a
Eric Amador <eric.amador@nimbisservices.com>
2014-10-10 15:48:32 -0400

    Pep8 fixes.

--- djangocms_vimeo/tests.py
+++ djangocms_vimeo/tests.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from cms.test_utils.testcases import CMSTestCase
 from django.test import TestCase
 
+
 class VimeoTestCase(CMSTestCase):
 
     def setUp(self):
